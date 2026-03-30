881765db78271369814e0e1428f1f8f763431220
Lucas Jidenius <jidenius@live.se>
2026-03-30 20:26:49 +0000

    Knapp tryck och tända lampa i pico

--- interrupt/src/interrupt.c
+++ interrupt/src/interrupt.c
@@ -1,14 +1,26 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
+#include "pico/cyw43_arch.h"
 
 #define BUTTON_PIN 15
 
+static bool led_on = false;
+
 void button_callback(uint gpio, uint32_t events) {
-    printf("PRESSED\n");
+    led_on = !led_on;
+    // På W-modeller styr vi LED via Wi-Fi-chippet
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
+    
+    printf("PRESSED -> LED is now %s\n", led_on ? "ON" : "OFF");
 }
 
 int main() {
     stdio_init_all();
+
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed\n");
+        return -1;
+    }
 
     gpio_init(BUTTON_PIN);
     gpio_set_dir(BUTTON_PIN, GPIO_IN);
@@ -16,7 +28,7 @@ int main() {
 
     gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_FALL, true, &button_callback);
 
-    printf("Starting...\n");
+    printf("Pico 2 W Interrupt Ready!\n");
 
     while (true) {
         tight_loop_contents();
